fcb68fcd013f24d9f86339d66998e1a903e86bf3
Sreenija Earanki <72650794+sreenijaearanki@users.noreply.github.com>
2025-08-07 22:08:59 -0400

    Time: 0 ms (100.00%) | Memory: 19 MB (23.30%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # use a map and check if the target - curr elem is in the map, if there then return those 2 indices
+
         hm={}
         for i,num in enumerate(nums):
-            elem = target - num
-            if elem in hm:
-                return [hm[elem],i]
+            if target-num in hm:
+                return [hm[target-num], i]
             hm[num]=i
-        return []
